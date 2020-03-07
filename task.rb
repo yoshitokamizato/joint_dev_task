e074292c7a625e65ece8f56837ba01c5b6a66519
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2020-03-07 16:26:45 +0900

    q1q2q3q4q5

--- task.rb
+++ task.rb
@@ -5,7 +5,8 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names << "斎藤"
+  puts names
 end
 
 def q2
@@ -13,21 +14,22 @@ def q2
   array2 = %w(bird bat tiger)
 
   # 以下に回答を記載
-
+  array = array1 + array2
+  p array
 end
 
 def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
-
+  puts numbers.count(3)
 end
 
 def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+  p sports.compact!
 end
 
 def q5
@@ -35,7 +37,8 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  p array1.empty?
+  p array2.empty?
 end
 
 def q6
