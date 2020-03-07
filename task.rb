b32e60dfa7776c7e3a72fe366de28a9d29a0f038
kuwahara <kuwahara@MacBook-no-MacBook-Pro.local>
2020-03-07 10:13:21 +0900

    add Q2~Q4 to task.rb

--- task.rb
+++ task.rb
@@ -22,14 +22,15 @@ def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
-
+  p numbers.select{3}
 end
 
 def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+  sports.compact!
+  p sports
 end
 
 def q5
