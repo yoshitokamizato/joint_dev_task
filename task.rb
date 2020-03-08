24ebec2d0bbd32b9a5a27333ff722a32920b859a
Kota Tsunodae <tsunoda.0688k@outlook.jp>
2020-03-08 12:32:26 +0900

    update-Q11

--- task.rb
+++ task.rb
@@ -65,7 +65,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -75,6 +76,9 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+  names.each.with_index(1)do |name,i|
+    puts "会員NO,#{i} #{name}さん"
+  end
 
 end
 
@@ -82,13 +86,25 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  foods.each do |food|
+    if food.include?("うに")
+      puts "好物"
+    else
+      puts "まぁまぁ好きです"
+    end
+  end
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
+  sports.flatten!
+  sports.uniq!
+  sports.each.with_index(1)do |sport, index|
+    puts "NO#{index} #{sport}"
+  end
+
 
 end
 
