bd7e412f282f76ee4b7d5491fcdb825c6cd91b61
kuwahara <kuwahara@MacBook-no-MacBook-Pro.local>
2020-03-10 08:34:56 +0900

    Q11とQ12の回答

--- task.rb
+++ task.rb
@@ -94,14 +94,19 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-
+    puts "ユーザーの趣味一覧"
+    sports.flatten!.uniq!
+    sports.each.with_index(1) do|sport,i| 
+    puts "No.#{i} #{sport}"
+  end
 end
+
 
 def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
-
+    puts data.dig(:user,:name)
 end
 
 def q13
