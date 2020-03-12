2f53575ea91d87e8da0c270fb4b426c452a7f4be
kuwahara <kuwahara@MacBook-no-MacBook-Pro.local>
2020-03-12 09:03:48 +0900

    Q13~Q16を回答

--- task.rb
+++ task.rb
@@ -113,15 +113,17 @@ def q13
   user_data = { name: "神里", age: 31, address: "埼玉" }
   update_data = { age: 32, address: "沖縄" }
 
-  # 以下に回答を記載
-
+   # 以下に回答を記載
+  user_data.merge!(update_data)
+  puts user_data
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  array = data.keys
+  puts array
 end
 
 def q15
@@ -129,6 +131,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
+  puts data1.has_key?(:age) ? 'OK' : 'NG'
+  puts data2.has_key?(:age) ? 'OK' : 'NG'
 
 end
 
@@ -141,7 +145,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.each do |user|
+   puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"
+  end
 end
 
 class UserQ17
