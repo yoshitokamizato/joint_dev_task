b8b7dd8ab3944f2d60643e7da4f756b0e598f89a
kasyu-create <63037931+kasyu-create@users.noreply.github.com>
2020-04-08 15:18:54 +0900

    add-ruby

--- task.rb
+++ task.rb
@@ -48,8 +48,8 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-  numbers1.map!{|numbers| numbers*10}
-  puts numbers1
+  numbers2 = numbers1.map{|numbers| numbers*10}
+  puts numbers2
 end
 
 def q7
@@ -65,8 +65,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
-  upper_case_programming_languages = programming_languages.map!(&:capitalize)
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
