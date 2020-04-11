785502d90923e461cc96a8b23ba92ce76cac80d5
shiba920sof <45893677+shiba920sof@users.noreply.github.com>
2020-04-12 01:23:06 +0900

    PHP仕様に変更
    PHP仕様に変更

---
+++ task.php
@@ -0,0 +1,197 @@
+# 課題の回答は このファイル をご利用下さい。
+# 回答の出力を確認される際は，「php task.php」をターミナルから実行して下さい。
+
+print("#####q1#####".PHP_EOL);
+$names = ["田中", "佐藤", "佐々木", "高橋"];
+
+  # 以下に回答を記載
+
+echo PHP_EOL;
+
+print("#####q2#####".PHP_EOL);
+$array1 = ["dog", "cat", "fish"];
+$array2 = ["bird", "bat", "tiger"];
+
+  # 以下に回答を記載
+
+echo PHP_EOL;
+
+print("#####q3#####".PHP_EOL);
+$numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9];
+
+  # 以下に回答を記載
+
+echo PHP_EOL;
+
+print("#####q4#####".PHP_EOL);
+$sports = ["サッカー", "フットサル", null, "野球", "バスケ", null, "バレー"];
+
+  # 以下に回答を記載
+
+echo PHP_EOL;
+
+print("#####q5#####".PHP_EOL);
+$array1 = [];
+$array2 = [1, 5, 8, 10];
+
+  # 以下に回答を記載
+
+echo PHP_EOL;
+
+print("#####q6#####".PHP_EOL);
+$numbers1 = [1, 2, 3, 4, 5];
+
+  # 以下に回答を記載
+
+echo PHP_EOL;
+
+print("#####q7#####".PHP_EOL);
+$array = ["1", "2", "3", "4", "5"];
+
+  # 以下に回答を記載
+
+echo PHP_EOL;
+
+print("#####q8#####".PHP_EOL);
+$programming_languages = ["php","ruby","python","javascript"];
+
+  # 以下に回答を記載
+
+  # 以下は変更しないで下さい
+print_r($programming_languages);
+echo PHP_EOL;
+print_r($upper_case_programming_languages);
+
+echo PHP_EOL;
+
+print("#####q9#####".PHP_EOL);
+$names = ["田中", "佐藤", "佐々木", "高橋"];
+
+  # 以下に回答を記載
+
+echo PHP_EOL;
+
+print("#####q10#####".PHP_EOL);
+$foods = ["いか","たこ","うに","しゃけ","うにぎり","うに軍艦","うに丼"];
+
+  # 以下に回答を記載
+
+echo PHP_EOL;
+
+print("#####q11#####".PHP_EOL);
+$sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]];
+
+  # 以下に回答を記載
+
+echo PHP_EOL;
+
+print("#####q12#####".PHP_EOL);
+$data = [ "user" => [ "name" => "satou", "age" => 33 ] ];
+
+  # 以下に回答を記載
+
+echo PHP_EOL;
+
+print("#####q13#####".PHP_EOL);
+$user_data = [ "name" => "神里", "age" => 31, "address" => "埼玉"];
+$update_data = [ "age" => 32, "address" => "沖縄" ];
+
+  # 以下に回答を記載
+
+echo PHP_EOL;
+
+print("#####q14#####".PHP_EOL);
+$data = [ "name" => "satou", "age" => 33, "address" => "saitama", "hobby" => "soccer", "email" => "hoge@fuga.com" ];
+
+  # 以下に回答を記載
+
+echo PHP_EOL;
+
+print("#####q15#####".PHP_EOL);
+$data1 = [ "name" => "saitou", "hobby" => "soccer", "age" => 33, "role" => "admin" ];
+$data2 = [ "name" => "yamada", "hobby" => "baseball", "role" => "normal" ];
+
+  # 以下に回答を記載
+
+echo PHP_EOL;
+
+print("#####q16#####".PHP_EOL);
+$users = [
+  [ "name" => "satou", "age" => 22 ],
+  [ "name" => "yamada", "age" => 12 ],
+  [ "name" => "takahashi", "age" => 32 ],
+  [ "name" => "nakamura", "age" => 41 ]
+];
+
+  # 以下に回答を記載
+
+echo PHP_EOL;
+
+class UserQ17
+  # 以下に回答を記載
+
+echo PHP_EOL;
+
+print("#####q17#####".PHP_EOL);
+  # ここは変更しないで下さい（ユーザー情報は変更していただいてOKです）
+  user1 = UserQ17.new(name: "神里", age: 32, gender: "男")
+  user2 = UserQ17.new(name: "あじー", age: 32, gender: "男")
+
+  user1.info
+  puts "-------------"
+  user2.info
+echo PHP_EOL;
+
+class UserQ18
+  # 以下に回答を記載
+
+echo PHP_EOL;
+
+print("#####q18#####".PHP_EOL);
+  # ここは変更しないで下さい
+  user1 = UserQ18.new(name: "あじー", age: 32)
+  user2 = UserQ18.new(name: "ゆたぼん", age: 10)
+
+  puts user1.introduce
+  puts user2.introduce
+echo PHP_EOL;
+
+class Item
+  # 以下を修正して下さい
+
+  def initialize(name)
+    @name = name
+  end
+echo PHP_EOL;
+
+print("#####q19#####".PHP_EOL);
+  # ここは変更しないで下さい
+  book = Item.new(name: "ゼロ秒思考")
+  puts book.name
+echo PHP_EOL;
+
+class UserQ20
+  # 以下に回答を記載
+
+echo PHP_EOL;
+
+class Zoo
+  # 以下に回答を記載
+
+echo PHP_EOL;
+
+
+print("#####q20#####".PHP_EOL);
+  # ここは変更しないで下さい（動物園・ユーザー情報は変更していただいてOKです）
+  zoo = Zoo.new(name: "旭山動物園", entry_fee: { infant: 0, children: 400, adult: 800, senior: 500 })
+
+  users = [
+    UserQ20.new(name: "たま", age: 3),
+    UserQ20.new(name: "ゆたぼん", age: 10),
+    UserQ20.new(name: "あじー", age: 32),
+    UserQ20.new(name: "ぎん", age: 108)
+  ]
+
+  users.each do |user|
+    zoo.info_entry_fee(user)
+echo PHP_EOL;
