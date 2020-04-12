d0a13f070b87fa6f96ebcdd2b5282e03a7b527ba
shiba920sof <45893677+shiba920sof@users.noreply.github.com>
2020-04-12 09:39:07 +0900

    PHP仕様に変更
    PHP仕様に変更

--- task_ansewr.php
+++ task_ansewr.php
@@ -37,7 +37,7 @@ foreach($numbers as $number){
       $count++;
    }
 }
-print_r($count . "回");
+print_r($count . "回".PHP_EOL);
 
 echo PHP_EOL;
 
@@ -56,7 +56,7 @@ echo PHP_EOL;
 
 print("#####q5#####".PHP_EOL);
 //Q5. 配列が空であれば true，1つ以上の要素があれば false を出力するコードを書いて下さい。
-//配列　空　判定
+//キーワード：配列　空　判定
 
 $array1 = [];
 var_export(!empty($array1));
@@ -243,6 +243,160 @@ $users = [
 
 foreach($users as $key => $user){
     echo "私の名前は".$user["name"]."です。年齢は".$user["age"]."歳です。".PHP_EOL;
+}
+
+echo PHP_EOL;
+
+
+print("#####q17#####".PHP_EOL);
+//Q17. 次の Userクラス 内にinfoメソッドを追加し，期待する出力結果になるようにして下さい。
+
+class User
+{
+    
+    protected $name;
+    protected $age;
+    protected $gender;
+    
+    function __construct($user_name,$user_age,$user_gender)    {
+        $this->name = $user_name;
+        $this->age = $user_age;
+        $this->gender = $user_gender;
+    }
+    
+    function info() {
+        print("名前:".$this->name.PHP_EOL);
+        print("年齢:".$this->age.PHP_EOL);
+        print("性別:".$this->gender.PHP_EOL);
+    }
+
+}
+
+$user1 = new User("神里",32,"男");
+$user2 = new User("あじー",32,"男");
+
+$user1->info();
+print("-------------".PHP_EOL);
+$user2->info();
+
+echo PHP_EOL;
+
+
+print("#####q18#####".PHP_EOL);
+//Q18. 年齢を用いた場合分けを利用して，期待する出力結果になるようなManクラスを作成して下さい。
+
+class Man
+{
+
+    protected $name;
+    protected $age;
+    
+    function __construct($user_name,$user_age)    {
+        $this->name = $user_name;
+        $this->age = $user_age;
+    }
+    
+    function introduce() {
+        if($this->age >= 20){
+            print("こんにちは,".$this->name."と申します。宜しくお願いいたします。".PHP_EOL);
+        }else{
+            print("はいさいまいど〜，".$this->name."です！！！".PHP_EOL);
+        }
+    }
+
+}
+
+$man1 = new Man("あじー",32);
+$man2 = new Man("ゆたぼん",10);
+
+$man1->introduce();
+$man2->introduce();
+
+echo PHP_EOL;
+
+
+print("#####q19#####".PHP_EOL);
+//Q19. 次のコードはエラーが出ます。期待する出力結果となるようにコードを修正して下さい。
+
+class Item{
+    # 以下を修正して下さい
+
+    public $name;
+    //protected $name;
+
+    function __construct($book_name){
+        $this->name = $book_name;
+    }
+}
+  # 以下は変更しないで下さい
+
+$book = new Item("ゼロ秒思考");
+print($book->name.PHP_EOL);
+
+echo PHP_EOL;
+
+
+print("#####q20#####".PHP_EOL);
+//Q20. 次の仕様を満たした上で，期待する出力結果になるようにコードを追加して下さい。
+//年齢区分は，幼児(0〜5歳)，子供(6〜12歳)，成人(13〜64歳)，シニア(65〜120歳)の4パターンとします。（この範囲外の年齢については対処しなくてOKです）
+
+class Human
+{
+
+    public $name;
+    public $age;
+    
+    function __construct($user_name,$user_age)    {
+        $this->name = $user_name;
+        $this->age = $user_age;
+    }
+    
+    function introduce() {
+        if($this->age >= 20){
+            print("こんにちは,".$this->name."と申します。宜しくお願いいたします。".PHP_EOL);
+        }else{
+            print("はいさいまいど〜，".$this->name."です！！！".PHP_EOL);
+        }
+    }
+
+}
+
+class Zoo
+{
+    
+    protected $name;
+    protected $entry_fee;
+    
+    function __construct($zoo_name,$zoo_entry_fee)    {
+        $this->name = $zoo_name;
+        $this->entry_fee = $zoo_entry_fee;
+    }
+    
+    function info_entry_fee(Human $human) {
+        if($human->age <= 5){
+            print($human->name."さんの入場料金は ".$this->entry_fee["infant"]." 円です。".PHP_EOL);
+        }elseif($human->age <= 12){
+            print($human->name."さんの入場料金は ".$this->entry_fee["children"]." 円です。".PHP_EOL);
+        }elseif($human->age <= 64){
+            print($human->name."さんの入場料金は ".$this->entry_fee["adult"]." 円です。".PHP_EOL);
+        }elseif($human->age <= 120){
+            print($human->name."さんの入場料金は ".$this->entry_fee["senior"]." 円です。".PHP_EOL);
+        }
+    }
+    
+}
+
+$zoo = new Zoo("旭山動物園",[ "infant" => 0, "children" => 400, "adult" => 800, "senior" => 500]);
+
+$human1 = new Human("たま",3);
+$human2 = new Human("ゆたぼん",10);
+$human3 = new Human("あじー",32);
+$human4 = new Human("ぎん",108);
+
+$humans = [ $human1, $human2, $human3, $human4 ];
+
+foreach($humans as $human){
+    $zoo->info_entry_fee($human);
 }
 
 echo PHP_EOL;
