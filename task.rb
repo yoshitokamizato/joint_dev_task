086672eb8cdca63abce965a86b3d76a3c70bc58d
Chibariyooh <62876107+Chibariyooh@users.noreply.github.com>
2020-04-27 21:39:01 +0900

    add q12q13 to task.rb

--- task.rb
+++ task.rb
@@ -106,7 +106,7 @@ def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
-
+  p data[:user][:name]
 end
 
 def q13
@@ -114,7 +114,8 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  user_data.merge!(update_data)
+  p user_data
 end
 
 def q14
