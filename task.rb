d640908d41e77b26e1af35bb18431f552d6980e3
SHINICHI <masaki.shinichi.1984@gmail.com>
2020-06-27 14:50:15 +0900

    add q1-q11

--- task.rb
+++ task.rb
@@ -47,13 +47,15 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+numbers1 = numbers1.map { |n| n * 10 }
+p numbers1
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
+array = array.map{|n| n.to_i}
 
   # 以下は変更しないで下さい
   p array
@@ -63,7 +65,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages = programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -73,21 +76,36 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  number = 1
+  names.each do |text|
+    puts "会員No.#{number} #{text}さん"
+    number += 1
+  end
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  if foods.any?{|food|"うに".include? food}
+    puts "好物です"
+  elsif
+    puts "まぁまぁ好きです"
+  end
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-
+  sports.flatten!
+  sports.uniq!
+  number = 1
+    puts "ユーザーの趣味一覧"
+  sports.each.with_index(1)do|sport,i|
+    puts "No.#{i} #{sport}"
+    number += 1
+  end
 end
 
 def q12
