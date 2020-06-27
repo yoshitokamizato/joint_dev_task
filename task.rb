47e4cdcff88d6cb2c1c7e88f405d96618724c56e
nakataniako <acco.yamato@gmail.com>
2020-06-28 05:43:16 +0900

    add_Q8_3

--- task.rb
+++ task.rb
@@ -74,8 +74,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  p programming_languages.map(&:capitalize)
-  p programming_languages.map(&:upcase)
+  programming_languages.map(&:capitalize!)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
   p programming_languages
