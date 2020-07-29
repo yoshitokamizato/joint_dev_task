75697a5ff37488520b0fd22fc6195bad7082d7e7
shogo2421 <2421falsecarp@gmail.com>
2020-07-29 22:23:13 +0900

    Q1~Q3回答

--- task.rb
+++ task.rb
@@ -5,7 +5,7 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  puts names.push("斎藤")
 end
 
 def q2
@@ -13,14 +13,15 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-
+  array1.push(array2)
+  puts array1
 end
 
 def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
-
+  puts numbers.count(3)
 end
 
 def q4
