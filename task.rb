1662fb2ca16a58547e78203a4fa1e7b87b880765
shogo2421 <2421falsecarp@gmail.com>
2020-08-01 22:43:13 +0900

    q12-q16

--- task.rb
+++ task.rb
@@ -114,14 +114,16 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  p user_data.merge(update_data)
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  data.each_key do |key|
+    puts "#{key}"
+  end
 end
 
 def q15
@@ -129,7 +131,18 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
+  if data1.has_key?(:age)
+    puts "OK"
+  else
+    puts "NG"
+  end
 
+  if data2.has_key?(:age)
+    puts "OK"
+  else
+    puts "NG"
+  end
+    
 end
 
 def q16
@@ -141,7 +154,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}です。"
+  end
 end
 
 class UserQ17
