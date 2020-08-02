2f2716f8f5730eedc0c5c633b8cf056dc6a4aef0
shogo2421 <2421falsecarp@gmail.com>
2020-08-02 15:56:47 +0900

    q13-q16解答

--- task.rb
+++ task.rb
@@ -116,9 +116,7 @@ def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-  data.each_key do |key|
-    puts "#{key}"
-  end
+  puts data.keys
 end
 
 def q15
@@ -126,18 +124,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-  if data1.has_key?(:age)
-    puts "OK"
-  else
-    puts "NG"
-  end
-
-  if data2.has_key?(:age)
-    puts "OK"
-  else
-    puts "NG"
-  end
-    
+  puts data1.has_key?(:age) ? "OK" : "NG"
+  puts data2.has_key?(:age) ? "OK" : "NG"
 end
 
 def q16
