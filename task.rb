460c55e9f08bc050048d942a15c7afe3aa681b14
Maruko <hp_maru@icloud.com>
2020-09-06 19:18:13 +0900

    Answered Q8~Q10 to task.rb

--- task.rb
+++ task.rb
@@ -68,7 +68,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map{|s| s.capitalize!}
+  upper_case_programming_languages = programming_languages.map{|s| s.upcase}
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -78,14 +79,22 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each.with_index(1) do |name, i|
+      puts "会員No.#{i} #{name}さん"
+  end
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  foods.each do |food|
+    if food.include?("うに")
+      puts "好物です"
+    else
+      puts "まぁまぁ好きです"
+    end
+  end
 end
 
 def q11
