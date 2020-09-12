3ce15df272219dafbbb3e029e4329a80ebc2ef5b
nozomi <kkk@docomo.ne.jp>
2020-09-12 18:24:58 +0900

    #7 6完成

--- task.rb
+++ task.rb
@@ -47,10 +47,8 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-numbers1.each do |number|
-   numbers2=number*10
-   puts numbers2
-end
+  numbers2 = numbers1.map{|p| p*10}
+  p numbers2
 end
 
 def q7
