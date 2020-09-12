7cf6ba9f8c7b7d4470ab954a2230cb70a1eccca6
nozomi <kkk@docomo.ne.jp>
2020-09-12 09:42:35 +0900

    #master 1~6完成

--- task.rb
+++ task.rb
@@ -5,7 +5,8 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+names.push("斎藤")
+puts names
 end
 
 def q2
@@ -13,21 +14,22 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-
+array1.push(array2)
+puts array1
 end
 
 def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
-
+puts numbers.count(3)
 end
 
 def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+sports.delete(nil)
   # 以下は変更しないで下さい
   p sports
 end
@@ -37,14 +39,18 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+ puts array1.empty?
+ puts array2.empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+numbers1.each do |number|
+   numbers2=number*10
+   puts numbers2
+end
 end
 
 def q7
