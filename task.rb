c0a9104a70d1ee75e493ff23f91facb97033f240
nozomi <kkk@docomo.ne.jp>
2020-09-12 17:51:07 +0900

    #7 14完了

--- task.rb
+++ task.rb
@@ -138,7 +138,9 @@ def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+   data = { "name" => "satou", "age"=> 33, "address"=> "saitama", "hobby"=> "soccer", "email"=> "hoge@fuga.com" }
+  array=data.keys
+  p array
 end
 
 def q15
