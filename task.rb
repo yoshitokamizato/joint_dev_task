c400f0c6eb94438e0b32b05b5ea31da537913815
kurogi yuki <xtoriymoonz2@yahoo.co.jp>
2020-09-15 14:34:32 +0900

    add q6q10

--- task.rb
+++ task.rb
@@ -63,7 +63,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  x = programming_languages.map{ |x| x.capitalize!}
+  programming_languages.map{ |x| x.capitalize!}
   upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
