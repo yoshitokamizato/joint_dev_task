16af873987fb4c0d44ace32a590c198de3a007ef
Maruko-HP <hp_maru@icloud.com>
2020-09-23 22:15:52 +0900

    Q17のコードをオプション引数に修正しました

--- task.rb
+++ task.rb
@@ -170,10 +170,10 @@ class UserQ17
   # 以下に回答を記載
   # attr_accessor :name, :age, :gender
 
-  def initialize(name:, age:, gender:)
-    @name = name
-    @age = age
-    @gender = gender
+  def initialize(**params)
+    @name = params[:name]
+    @age = params[:age]
+    @gender = params[:gender]
   end
 
   def info
@@ -236,9 +236,9 @@ class UserQ20
   # 以下に回答を記載
   attr_reader :name, :age
 
-  def initialize(name:, age:)
-    @name = name
-    @age = age
+  def initialize(**params)
+    @name = params[:name]
+    @age = params[:age]
   end
 end
 
