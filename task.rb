2aa13591858eac0b7102e62c2cd33ff4137f97ab
tjoko <take.ehime@gmail.com>
2020-11-16 08:43:51 +0900

    Ruby基礎Q6~10

--- task.rb
+++ task.rb
@@ -51,14 +51,15 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+  numbers2 = numbers1.map {|num| num * 10}
+  p numbers2
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array.map! {|n| n.to_i}
   # 以下は変更しないで下さい
   p array
 end
@@ -67,7 +68,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -77,14 +79,22 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each.with_index(1) do |name, i|
+    puts "会員No.#{i} #{name}さん"
+  end
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  foods.each do |n|
+    if n.include?("うに")
+      puts "#{n}は好物です"
+    else
+      puts "#{n}はまぁまぁ好きです"
+    end
+  end
 end
 
 def q11
