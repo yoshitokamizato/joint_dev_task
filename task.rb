672fe4da80f4343588652a27e583ec6dfb0eaed4
yoshi <tomoharu11010115@gmail.com>
2020-12-10 17:10:20 +0900

    task_ruby

--- task.rb
+++ task.rb
@@ -59,8 +59,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-  # integers = array.map!(&:to_i)
-  array.map!{|num| num.to_i}
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -69,7 +68,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  
+
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
