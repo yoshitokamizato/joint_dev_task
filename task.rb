4fbcd2d59305c5e68d23db4884311e2d176a4597
KAIKI-T <ka.1.k1.0817@gmail.com>
2020-12-20 21:31:28 +0900

    commit message

--- task.rb
+++ task.rb
@@ -29,7 +29,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+newsports = sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -39,14 +39,16 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+puts array1.empty?
+puts array2.empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+numbers2 = numbers1.map {|n| n * 10}
+p numbers2
 end
 
 def q7
