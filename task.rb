0890af1103eab8f15f2af52e58cf05bf2d3a652f
KAIKI-T <ka.1.k1.0817@gmail.com>
2020-12-23 22:04:38 +0900

    commit message

--- task.rb
+++ task.rb
@@ -55,7 +55,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-array = ["1", "2", "3", "4", "5"].map(&:to_i)
+array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -63,7 +63,7 @@ end
 def q8
   programming_languages = %w(ruby php python javascript)
   # 以下に回答を記載
-programming_languages = %w(ruby php python javascript).map(&:capitalize)
+programming_languages.map!(&:capitalize)
 upper_case_programming_languages = programming_languages .map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
