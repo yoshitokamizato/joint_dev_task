cabefd9a9dfd0a67dbcd927bbc0d5806ad66b351
kakitomo235 <59069977+kakitomo235@users.noreply.github.com>
2020-12-31 08:03:55 +0900

    Q4Q6..Q9

--- task.rb
+++ task.rb
@@ -31,7 +31,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+  sports = sports.compact!
   # 以下は変更しないで下さい
   p sports.compact
 
@@ -51,13 +51,14 @@ def q6
 
   # 以下に回答を記載
 
+    p numbers2 = numbers1.map{|x| x * 10}
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array = array.map(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -66,6 +67,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages = programming_languages.map(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
   p programming_languages
@@ -73,11 +76,14 @@ def q8
 end
 
 def q9
+
   names = ["田中", "佐藤", "佐々木", "高橋"]
-
-  # 以下に回答を記載
-
+  #以下に回答を記載
+  names.each.with_index(1) do |name,i|
+  puts "会員No.#{i} #{name}さん"
+  end
 end
+
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
