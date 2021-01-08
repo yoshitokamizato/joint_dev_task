ad5fec1e815200188b760269b58b71b29059fa6f
miya <rinn.sasami@gmail.com>
2021-01-08 09:36:32 +0900

    first commit

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 # 課題の回答は task.rb をご利用下さい。
 # 回答の出力を確認される際は，「ruby main.rb」をターミナルから実行して下さい。
 
-require './management'
-require './message'
+require "./management"
+require "./message"
 
 management = Management.new
 management.prompt_input
