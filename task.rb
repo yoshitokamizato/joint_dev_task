7924b5ea935e93d9c592bdc9a0364889ae75d174
miya <rinn.sasami@gmail.com>
2021-01-11 15:57:25 +0900

    task add to task.rb

--- task.rb
+++ task.rb
@@ -118,14 +118,16 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  puts user_data.merge!(update_data)
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  data.each_value do |info|
+    puts info
+  end
 end
 
 def q15
@@ -133,7 +135,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  puts "OK" if data1.key?(:name)
+  puts "NO" if data2.key?(:name)
 end
 
 def q16
@@ -145,7 +148,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.each do |user| 
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"
+  end
 end
 
 class UserQ17
