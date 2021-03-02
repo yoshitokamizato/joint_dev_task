525925aab297cb4a39607650620add276b1e2755
Megurin <tutimur_0521@yahoo.co.jp>
2021-03-02 16:51:48 +0900

    q1からq4を回答

--- task.rb
+++ task.rb
@@ -5,6 +5,9 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+  names[4] = "斎藤"
+
+  p names
 
 end
 
@@ -13,6 +16,8 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
+  array = array1 + array2
+  p array
 
 end
 
@@ -20,6 +25,8 @@ def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
+  puts numbers.count(3)
+
 
 end
 
@@ -27,6 +34,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
+  sports.delete(nil)
 
   # 以下は変更しないで下さい
   p sports
