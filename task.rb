1e790ec6b78840156c8d230abfd85af4139b3153
yasu-grz <yatthun82@gmail.com>
2021-03-06 08:58:08 +0900

    Q8を解答

--- task.rb
+++ task.rb
@@ -66,7 +66,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
   
   # 以下に回答を記載
-
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
