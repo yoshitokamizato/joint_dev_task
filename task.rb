5581134e407fb98134ffa616713b8914e5d958d1
yasu-grz <yatthun82@gmail.com>
2021-04-13 15:25:40 +0900

    18を再修正、Q20を回答中

--- task.rb
+++ task.rb
@@ -232,21 +232,22 @@ class UserQ20
 end
 class Zoo
   # 以下に回答を記載
+  attr_reader :name, entry_fee:
   initialize (name:, entry_fee:)
     @entry_fee = entry_fee
 end
 #以下に条件分岐を作成（ifよりcaseの方が複数処理ができる）
 #「もし（0~5歳）なら(幼児)（処理１）、（6~12歳なら子供）（処理２）、（13~64歳）なら(成人)（処理３）、（65~120歳）なら(シニア)（処理４）と分ける。
 def users.each do |user|#条件式
-  fee = {age |fee|}#変数feeを定義したい
-  case  info_entry_fee(user)#変数userを使用（initializeで定義済）
+  fee = {}#変数feeを定義したい
+  case  zoo.info_entry_fee(user)#変数userを使用（initializeで定義済）
   when 0 .. 5 #処理1
     [infant: 0]
   when 6 .. 12 #処理2
     [children: 400]
   when 13 .. 64 #処理3
     [adult: 800]
-  when  65 .. 120 #処理4
+  when 65 .. 120 #処理4
     [senior: 500]
   end
   #戻り値を出力（@users.nameと@feeを戻り値にする)
