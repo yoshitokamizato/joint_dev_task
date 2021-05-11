a5182076018c62a38cc8f49edcb297f78118f8f4
damari <motokin128@gmail.com>
2021-05-11 18:46:13 +0900

    add Ruby_task

--- task.rb
+++ task.rb
@@ -66,11 +66,7 @@ def q8
 
   # 以下に回答を記載
   programming_languages.map!(&:capitalize)
-
-  upper_case_programming_languages = []
-  programming_languages.each do |i|
-    upper_case_programming_languages << i.upcase
-  end
+  upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
   p programming_languages
@@ -90,12 +86,14 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-  if foods.include?("うに")
-    puts "好物です"
-  else
-    puts "まぁまぁ好きです"
+  foods.each do |food|
+    if food.include?("うに")
+      puts "#{food}: 好物です"
+    else
+      puts "#{food}: まぁまぁ好きです"
+    end
   end
-
+  
 end
 
 def q11
