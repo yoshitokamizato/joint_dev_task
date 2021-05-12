f60d7c9b3ad5d5c521bb2304fce5a348516fb566
damari <motokin128@gmail.com>
2021-05-12 12:39:14 +0900

    add Ruby_task

--- task.rb
+++ task.rb
@@ -93,7 +93,7 @@ def q10
       puts "#{food}: まぁまぁ好きです"
     end
   end
-  
+
 end
 
 def q11
@@ -121,6 +121,7 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
+  p user_data.merge!(update_data)
 
 end
 
@@ -128,6 +129,8 @@ def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
+  array = data.keys
+  p array
 
 end
 
@@ -136,6 +139,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
+  puts data1.has_key?(:age)? "OK": "NG"
+  puts data2.has_key?(:age)? "OK": "NG"
 
 end
 
@@ -148,7 +153,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.each do  |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"
+  end
 end
 
 class UserQ17
