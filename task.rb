9d6dd9d007d0eaf1f414270a0bc28804686c8f28
damari <motokin128@gmail.com>
2021-05-20 01:45:09 +0900

    入場料金表示の処理を修正

--- task.rb
+++ task.rb
@@ -240,18 +240,19 @@ class Zoo
   def initialize(name:, entry_fee:)
     @entry_fee = entry_fee
   end
-
+  
   def info_entry_fee(user)
     case user.age
     when 0..5
-      puts "#{user.name}さんの入場料金は #{@entry_fee[:infant]} 円です。"
+      fee = @entry_fee[:infant]
     when 6..12
-      puts "#{user.name}さんの入場料金は #{@entry_fee[:children]} 円です。"
+      fee = @entry_fee[:children]
     when 13..64
-      puts "#{user.name}さんの入場料金は #{@entry_fee[:adult]} 円です。"
+      fee = @entry_fee[:adult]
     else 65..120
-      puts "#{user.name}さんの入場料金は #{@entry_fee[:senior]} 円です。"
+      fee = @entry_fee[:senior]
     end
+    puts "#{user.name}さんの入場料金は#{fee} 円です"
   end
 end
 
