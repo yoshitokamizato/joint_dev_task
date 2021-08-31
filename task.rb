0da946cad7bca36a7ff36484782f4db65673a309
R.moriyama <moriforest537@gmail.com>
2021-08-31 13:52:57 +0900

    Q10まで回答済み

--- task.rb
+++ task.rb
@@ -14,7 +14,7 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-  array3 = array1.concat(array2)
+  array3 = array1 + array2
   puts array3
 end
 
@@ -22,17 +22,15 @@ def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
-  three_value = numbers.find { |v| v == 3 }
-  puts three_value
+  count_three = numbers.find { |v| v == 3 }
+  puts count_three
 end
-
-q3
 
 def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+  sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -42,21 +40,23 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  puts "array1は#{array1.empty?}"
+  puts "array2は#{array2.empty?}"
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+  numbers2 = numbers1.map { |value| value * 10 }
+  puts numbers2
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array.map! { |value| value.to_i }
   # 以下は変更しないで下さい
   p array
 end
@@ -65,7 +65,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map! { |str| str.capitalize }
+  upper_case_programming_languages = programming_languages.map { |str| str.upcase }
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -75,14 +76,20 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each.with_index(1) do |name, i|
+    puts "会員No.#{i} #{name}さん"
+  end
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  if foods.include?("うに")
+    puts "好物です"
+  else
+    puts "まぁまぁ好きです"
+  end
 end
 
 def q11
