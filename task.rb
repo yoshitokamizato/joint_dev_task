420dd8c17d6722d6e8bb9802f41c8affaf561aa2
R.moriyama <moriforest537@gmail.com>
2021-08-31 14:35:29 +0900

    Q8の修正

--- task.rb
+++ task.rb
@@ -66,8 +66,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages.map! { |str| str.capitalize }
-  upper_case_programming_languages = programming_languages.map { |str| str.upcase }
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
